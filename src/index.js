956c56dc111545d10fe11a57ee316d1b1d3bd435
Kayla Vu <kaylaavu@gmail.com>
2024-02-10 14:38:21 -0800

    add subroutes for viewing specific category of products

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { UserProvider } from './contexts/user.context'
-import { ProductsProvider } from './contexts/products.context'
-import { BrowserRouter } from 'react-router-dom'
+import { UserProvider } from './contexts/user.context';
+import { CategoriesProvider } from './contexts/categories.context';
+import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './contexts/cart-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserProvider>
-      <ProductsProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-      </ProductsProvider>
-    </UserProvider>
+      <UserProvider>
+        <CategoriesProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </CategoriesProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
